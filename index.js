cad00c087119988a8f0842081c6ba1d277dd1da5
faishal3900 <faishal3900@gmail.com>
2025-08-02 00:59:52 +0530

    backend for deploy 2

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const { default: mongoose } = require("mongoose")
 const { MONGOURL } = require("./key")
 const cors=require('cors')
+const dotenv = require ("dotenv")
+
 
 app.use(express.json());
 app.use(cors())
 app.get("/", (req,res) => {
     res.send("this is get req")
 })
+dotenv.config()
+const PORT = process.env.PORT || 8000;
 
 
 mongoose.connect(MONGOURL)
@@ -18,7 +22,6 @@ app.use(require("./controllers/auth"))
 app.use(require("./controllers/user"))
 app.use(require("./controllers/post"))
 
-const port = 8000;
-app.listen(port, () => {
-    console.log(`Serve is running on port http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Serve is running on port http://localhost:${PORT}`);
 })
