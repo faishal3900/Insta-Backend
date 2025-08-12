eb6ce547e0e2ee0c8eb79855bcb205c1311333fb
faishal3900 <faishal3900@gmail.com>
2025-08-12 13:03:55 +0530

    cors thik kiya

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
-const express = require("express")
-const app = express();
-const { default: mongoose } = require("mongoose")
-const { MONGOURL } = require("./key")
-const cors=require('cors')
+// const express = require("express")
+// const app = express();
+// const { default: mongoose } = require("mongoose")
+// const { MONGOURL } = require("./key")
+// const cors = require('cors')
 
 
 
-app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:5173/","https://insta-frontend-ashen.vercel.app","http://localhost:5174/",],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+// app.use(express.json());
+// app.use(cors({
+//   origin: ["http://localhost:5173","https://insta-frontend-ashen.vercel.app","http://localhost:5174",],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
 
 
 // app.options("*", cors());
 
 
-app.get("/", (req,res) => {
-    res.send("this is get req")
-})
+// app.get("/", (req,res) => {
+//     res.send("this is get req")
+// })
 
-const PORT = 3000;
+// const PORT = 3000;
 
 
+// mongoose.connect(MONGOURL)
+// .then(() => console.log("Database connected successfully"));
+
+// app.use(require("./controllers/auth"))
+// app.use(require("./controllers/user"))
+// app.use(require("./controllers/post"))
+
+// app.listen(PORT , "0.0.0.0", () => {
+//     console.log(`Serve is running on port http://localhost:${PORT}`);
+// })
+
+
+const express = require("express");
+const app = express();
+const { default: mongoose } = require("mongoose");
+const { MONGOURL } = require("./key");
+const cors = require("cors");
+
+// Middleware
+app.use(express.json());
+
+// ✅ Fixed CORS configuration
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://insta-frontend-ashen.vercel.app",
+    "http://localhost:5174"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+// ✅ Allow preflight (OPTIONS) requests
+//app.options("*", cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("this is get req");
+});
+
+// Connect to MongoDB
 mongoose.connect(MONGOURL)
-.then(() => console.log("Database connected successfully"));
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
-app.use(require("./controllers/auth"))
-app.use(require("./controllers/user"))
-app.use(require("./controllers/post"))
+// Import Routes
+app.use(require("./controllers/auth"));
+app.use(require("./controllers/user"));
+app.use(require("./controllers/post"));
 
-app.listen(PORT , "0.0.0.0", () => {
-    console.log(`Serve is running on port http://localhost:${PORT}`);
-})
+// Start server
+const PORT = 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
